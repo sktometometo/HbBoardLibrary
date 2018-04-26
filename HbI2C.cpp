c7181a39586e5bbd33ceea393661789ac0078600
sktometometo <sktometometo@gmail.com>
2018-04-26 12:28:07 +0900

    BLEモジュールとの通信を実装

--- HbI2C.cpp
+++ HbI2C.cpp
@@ -5,58 +5,72 @@
 extern int HbSensorVal;
 
 // I2C Buffer
-uint8_t bufcmd[I2C_BUFSIZE];
-uint8_t bufdata[I2C_BUFSIZE];
+uint8_t bufrx[I2C_BUFSIZE];
+uint8_t buftx[I2C_BUFSIZE];
+
+// cmd and state
+int cmd; // if -1, received data is cmd;
+
+// state value
+int BLEStateVal;
 
 void requestEvent() {
-
-    for (int i=0; i<I2C_BUFSIZE || Wire.avilable(); i++) {
-        bufcmd[i] = Wire.read();
-    }
-    int cmd = (bufcmd[0] << 8) + bufcmd[1];
-
-    switch (cmd) {
-        case I2C_CMD_READ_STATE:
-            HbI2C_transceiveState();
-            break;
-        case I2C_CMD_READ_BAT:
-            HbI2C_transceiveBattery();
-            break;
+    if (cmd == -1) {
+        // if this section is excuted,
+        // something go wrong
+    } else {
+        switch (cmd) {
+            case I2C_CMD_READ_STATE:
+                HbI2C_transceiveState(0);
+                break;
+            case I2C_CMD_READ_BAT:
+                HbI2C_transceiveBattery(0);
+                break;
+        }
+        cmd = -1;
     }
 }
 
 void receiveEvent() {
     for (int i=0; i<I2C_BUFSIZE || Wire.avilable(); i++) {
-        bufcmd[i] = Wire.read();
-    }
-    for (int i=0; i<I2C_BUFSIZE || Wire.available(); i++) {
-        bufdata[i] = Wire.read();
+        bufrx[i] = Wire.read();
     }
 
-    int cmd = (bufcmd[0] << 8) + bufcmd[1];
-
-    switch (cmd) {
-        case I2C_CMD_SEND_SENSOR:
-            HbI2C_receiveSensor( (bufdata[0] << 8) + bufdata[1] );
-            break;
-        case I2C_CMD_SEND_BLESTATE:
-            HbI2C_receiveBLEState( bufdata[0] );
-            break;
+    if (cmd == -1) {
+        cmd = (bufcmd[0] << 8) + bufcmd[1];
+    } else {
+        switch (cmd) {
+            case I2C_CMD_SEND_SENSOR:
+                HbI2C_receiveSensor( (bufrx[0] << 8) + bufrx[1] );
+                break;
+            case I2C_CMD_SEND_BLESTATE:
+                HbI2C_receiveBLEState( bufrx[0] );
+                break;
+        }
+        cmd = -1;
     }
 }
 
-void HbI2C_transceiveState() {
+void HbI2C_transceiveState(uint8_t val) {
     // TBD
+    // TODO: bufdataを1バイトでつくる
+    buftx[0] = val
+    Wire.write(buftx, 1);
 }
 
-void HbI2C_transceiveBattery() {
+void HbI2C_transceiveBattery(uint16_t val) {
     // TBD
+    // TODO: bufdataを2バイトでつくる
+    buftx[0] = val >> 8;
+    buftx[1] = val;
+    Wire.write(buftx, 2);
 }
 
 void HbI2C_receiveSensor(int val) {
     HbSensorVal = val;
 }
 
-void HbI2C_receiveBLEState(int ) {
+void HbI2C_receiveBLEState(int val) {
     // TBD
+    BLEStateVal = val;
 }
